f198e787b628190f22a0460a19178b1e13669599
obidano <omotetedan2@gmail.com>
2021-06-29 18:56:47 +0100

    services, routing, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { MypageComponent } from './pages/mypage/mypage.component';
+import {AppComponent} from './app.component';
+import {MypageComponent} from './pages/mypage/mypage.component';
 import {FormsModule} from "@angular/forms";
-import { ProductComponent } from './pages/product/product.component';
-import { FormComponent } from './pages/form/form.component';
+import {ProductComponent} from './pages/product/product.component';
+import {FormComponent} from './pages/form/form.component';
 import {ProductService} from "./product.service";
-import {AppRoutingModule} from "./app-routing.module";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from "./modules/app-routing.module";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from "./modules/material.module";
+import { HomeComponent } from './pages/home/home.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './pages/training/training.component';
+import { CurrentTrainingComponent } from './pages/current-training/current-training.component';
+import { NewTrainingComponent } from './pages/new-training/new-training.component';
+import { PastTrainingComponent } from './pages/past-training/past-training.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MypageComponent,
     ProductComponent,
-    FormComponent
+    FormComponent,
+    HomeComponent,
+    SignUpComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/modules/app-routing.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {MypageComponent} from "../pages/mypage/mypage.component";
+import {FormComponent} from "../pages/form/form.component";
+import {HomeComponent} from "../pages/home/home.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'my-app', component: MypageComponent},
+  {path: 'form', component: FormComponent},
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}

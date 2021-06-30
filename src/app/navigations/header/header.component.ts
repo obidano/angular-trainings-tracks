fcc47f9247cc79a5cd1625da067dcda0e4021cb9
obidano <omotetedan2@gmail.com>
2021-06-30 13:12:55 +0100

    auth restrictions

--- src/app/navigations/header/header.component.ts
+++ src/app/navigations/header/header.component.ts
@@ -1,19 +1,26 @@
-import {EventEmitter, Output} from '@angular/core';
+import {EventEmitter, OnDestroy, Output} from '@angular/core';
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Output() toggle = new EventEmitter()
+  authListener = new Subscription()
+  isAuth?: boolean
+
 
   constructor(private auth: AuthService) {
   }
 
   ngOnInit(): void {
+    this.authListener = this.auth.isAuthChanged.subscribe(status => {
+      this.isAuth = status;
+    });
   }
 
   onToggle() {
@@ -22,5 +29,9 @@ export class HeaderComponent implements OnInit {
 
   onLogout() {
     this.auth.logout()
+  }
+
+  ngOnDestroy(){
+    this.authListener.unsubscribe();
   }
 }
--- src/app/navigations/sidenav/sidenav.component.ts
+++ src/app/navigations/sidenav/sidenav.component.ts
@@ -1,19 +1,26 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
   @Output() close = new EventEmitter()
+  authListener = new Subscription()
+  isAuth?: boolean
+
 
 
   constructor(private auth: AuthService) {
   }
 
   ngOnInit(): void {
+    this.authListener = this.auth.isAuthChanged.subscribe(status => {
+      this.isAuth = status;
+    });
   }
 
   onClose() {
@@ -24,5 +31,9 @@ export class SidenavComponent implements OnInit {
     this.onClose();
     this.auth.logout()
 
+  }
+
+  ngOnDestroy() {
+    this.authListener.unsubscribe();
   }
 }
